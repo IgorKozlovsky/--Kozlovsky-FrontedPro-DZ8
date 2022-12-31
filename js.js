199122a2e0eed0ebe7c95aa09ebf0ffbbeb22e62
Игорь Козловский <i.kozlovsky2004@gmail.com>
2022-12-31 21:12:14 +0200

    1

---
+++ js.js
@@ -0,0 +1,22 @@
+// let numOrStr = prompt("input number or string");
+// console.log(numOrStr);
+
+// switch (isNaN(numOrStr) || numOrStr) {
+//   case true:
+//     console.log(" number is Ba_NaN");
+//     break;
+//   case null:
+//     console.log("ви скасували");
+//     break;
+//   case "":
+//     console.log("Empty String");
+//     break;
+//   default:
+//     console.log("OK!");
+//     break;
+// }
+// "asda" ==
+
+let i = prompt("");
+i = i == null ? "error" : i.split(" ");
+console.log(i);
